74d29488d6974a99af15a4d5edb30bea0f0fdce3
QambarAli1 <qamberali4904@gmail.com>
2021-09-09 23:12:16 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  localStorage.clear()
   const appid = '973497d219f1c7351022d3f0bbbb2724';
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState(null);
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  }
+  const [iframesrc, setiframesrc] = useState(null)
 
-  function showPosition(position) {
-    console.log(
-      "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude
-    );
-    localStorage.setItem('lat', position.coords.latitude)
-    localStorage.setItem('long', position.coords.longitude)
-  }
-  getLocation()
+  // getLocation()
   useEffect(() => {
     if (city != null) {
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`)
@@ -35,18 +20,40 @@ function App() {
         })
     }
     else {
-      var lat = JSON.parse(localStorage.getItem('lat'))
-      var long = JSON.parse(localStorage.getItem('long'))
-
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${appid}&units=metric`)
-        .then(function (response) {
-          const newWeather = response.data
-          setWeather(newWeather)
-        })
-        .catch((error) => {
-          console.log('error occured ', error);
-        })
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+          console.log("Geolocation is not supported by this browser.");
+        }
+      }
+      function showPosition(position) {
+        console.log(`latitude = ${position.coords.latitude}`);
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${appid}&units=metric`)
+          .then(function (response) {
+            const newWeather = response.data
+            setWeather(newWeather)
+          })
+          .catch((error) => {
+            console.log('error occured ', error);
+          })
+      }
+      getLocation()
     }
+    if (weather != null) {
+      var descp = weather.weather[0].description
+      console.log(weather.weather[0].description);
+    }
+    if(descp=='overcast clouds' || descp=='smoke'){
+      console.log('Raining');
+    }
+    if(descp!='cloud'){
+      console.log(' ! rain');
+      var newiframeSrc = 'Hello'
+      setiframesrc(newiframeSrc)
+    }
+    console.log(iframesrc);
+
 
   }, [city])
 
@@ -58,9 +65,9 @@ function App() {
         <br />
         <input type="text" name="city" id="city" />
         <button
-        onClick= {()=>{
-          setCity(document.getElementById('city').value)
-        }}
+          onClick={() => {
+            setCity(document.getElementById('city').value)
+          }}
         >
           Search</button>
         <br /> <br />
@@ -71,8 +78,8 @@ function App() {
           (weather !== null) ?
             <>
               <h3> {weather.name} </h3>
+              <span className='temp'>{weather.main.temp} <sup>o</sup> C</span>
               <h2> {weather.weather[0].description}  <br />
-                <span className='temp'>{weather.main.temp} <sup>o</sup> C</span>
               </h2>
             </>
             :
